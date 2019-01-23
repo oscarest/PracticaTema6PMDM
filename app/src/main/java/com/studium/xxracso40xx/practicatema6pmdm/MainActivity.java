a63f9be75f3d537740d3327f72c0db6fbee8e917
Xxracso40xX <Xxracso40xX@gmail.com>
2019-01-23 22:35:19 +0100

    Initial commit

---
+++ app/src/main/java/com/studium/xxracso40xx/practicatema6pmdm/MainActivity.java
@@ -0,0 +1,14 @@
+package com.studium.xxracso40xx.practicatema6pmdm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    
+}
