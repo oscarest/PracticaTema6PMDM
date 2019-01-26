3e7f0ffa95321ce22e61031de6dba0f3ab884201
Xxracso40xX <Xxracso40xX@gmail.com>
2019-01-26 22:52:41 +0100

    Avance de la pantalla de personas

--- app/src/main/java/com/studium/xxracso40xx/practicatema6pmdm/Personas.java
+++ app/src/main/java/com/studium/xxracso40xx/practicatema6pmdm/Personas.java
@@ -1,5 +1,6 @@
 package com.studium.xxracso40xx.practicatema6pmdm;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -10,7 +11,9 @@ import android.support.annotation.RequiresApi;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.menu.MenuBuilder;
 import android.view.ContextMenu;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
@@ -21,6 +24,7 @@ import static android.Manifest.permission.CALL_PHONE;
 public class Personas extends AppCompatActivity {
 ImageView imagen1, imagen2, imagen3, imagen4, imagen5, imagen6;
     SharedPreferences prefs;
+    public static String cadena2="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -41,11 +45,19 @@ ImageView imagen1, imagen2, imagen3, imagen4, imagen5, imagen6;
         registerForContextMenu(imagen5);
         registerForContextMenu(imagen6);
     }
-
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
         getMenuInflater().inflate(R.menu.menupersonas, menu);
+    }
+    @SuppressLint("RestrictedApi")
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.menueditar, menu);
+        MenuBuilder m = (MenuBuilder) menu;
+        m.setOptionalIconsVisible(true);
+        return true;
     }
 
     @RequiresApi(api = Build.VERSION_CODES.M)
@@ -57,26 +69,35 @@ ImageView imagen1, imagen2, imagen3, imagen4, imagen5, imagen6;
         switch(item.getItemId())
         {
             case R.id.opcion4:cadena = prefs.getString("telefono1", null);
-            case R.id.opcion5: break;
+                if(cadena==null)
+                {
+                    //AQUÍ UN TOAST DICIENDO QUE ESTÁ VACÍO
+                    Toast.makeText(this, "Está vacio el campo teléfono/correo", Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+                    if (ContextCompat.checkSelfPermission(getApplicationContext(), CALL_PHONE) == PackageManager.PERMISSION_GRANTED)
+                    {
+                        intent.setData(Uri.parse(cadena));
+                        startActivity(intent);
+                    }
+                    else
+                    {
+                        requestPermissions(new String[]{CALL_PHONE}, 1);
+                    }
+                }
+                break;
+            case R.id.opcion5:
+                break;
         }
-        if(cadena==null)
-        {
-            //AQUÍ UN TOAST DICIENDO QUE ESTÁ VACÍO
-            Toast.makeText(this, "Está vacio el campo teléfono/correo", Toast.LENGTH_LONG).show();
-        }
-        else
-        {
-            if (ContextCompat.checkSelfPermission(getApplicationContext(), CALL_PHONE) == PackageManager.PERMISSION_GRANTED)
-            {
-                intent.setData(Uri.parse(cadena));
-                startActivity(intent);
-            }
-            else
-            {
-                requestPermissions(new String[]{CALL_PHONE}, 1);
-            }
-        }
-
         return super.onContextItemSelected(item);
+    }
+    @SuppressLint("NewApi")
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        Intent intent2 = new Intent(this, EditarPersonas.class);
+        startActivity(intent2);
+        return false;
     }
 }
